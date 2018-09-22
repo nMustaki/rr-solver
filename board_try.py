b0dc6ca2067a8e96cded98e02a127bc9f6d2a0fb
Nathan Mustaki <feydaykyn@gmail.com>
2018-09-22 09:07:09 +0200

    code is drier

--- board_try.py
+++ board_try.py
@@ -23,31 +23,11 @@ class BoardTry:
         self,
         goal_position: tuple,
         goal: constants.Goal,
-        main_robot: constants.Robot,
-        other_robots: typing.List[constants.Robot],
-        seen_list: set,
+        robots: typing.List[constants.Robot],
+        seen_positions: set,
     ):
-        for direction in constants.Direction:
-            try:
-                current_try = BoardTry(self.result_board, main_robot, direction)
-                if current_try.is_successful(goal_position):
-                    return current_try
-                if (
-                    current_try.result_board.robot_positions[main_robot.value]
-                    != self.result_board.robot_positions[main_robot.value]
-                ):
-                    sorted_positions = tuple(
-                        sorted(
-                            current_try.result_board.robot_positions,
-                            key=lambda k: [k[1], k[0]],
-                        )
-                    )
-                    if sorted_positions not in seen_list:
-                        self.leafs.append(current_try)
-                        seen_list.add(sorted_positions)
-            except ImpossibleMove:
-                pass
-        for robot in other_robots:
+
+        for robot in robots:
             for direction in constants.Direction:
                 try:
                     current_try = BoardTry(self.result_board, robot, direction)
@@ -63,9 +43,9 @@ class BoardTry:
                                 key=lambda k: [k[1], k[0]],
                             )
                         )
-                        if sorted_positions not in seen_list:
+                        if sorted_positions not in seen_positions:
                             self.leafs.append(current_try)
-                            seen_list.add(sorted_positions)
+                            seen_positions.add(sorted_positions)
                 except ImpossibleMove:
                     pass
         return None
--- game.py
+++ game.py
@@ -20,10 +20,9 @@ class Game:
         for goal in self._goals:
             print("Resolving " + str(goal))
             current_board.display(self.goal_positions, goal)
-            current_board, nb_turns = self.resolve_goal(
+            current_board = self.resolve_goal(
                 current_board, goal, self.goal_positions[goal]
             )
-            print("...resolved in {} turns".format(nb_turns + 1))
             print("")
             print("")
             print("")
@@ -31,16 +30,15 @@ class Game:
     def resolve_goal(
         self, start_board: Board, goal: constants.Goal, goal_position: tuple
     ):
-        other_robots = []
-        try:
+        if goal != constants.Goal.ANY:
             main_robot = constants.GOAL_TO_ROBOT[goal]
-            other_robots = [x for x in list(constants.Robot) if x != main_robot]
-        except KeyError:
-            main_robot, *other_robots = list(constants.Robot)
+            robots = sorted(constants.Robot, key=lambda k: 0 if k == main_robot else 1)
+        else:
+            robots = list(constants.Robot)
 
         current_tries = [BoardTry(start_board, None, None)]
         nb_turns = 0
-        seen_list = set()
+        seen_positions = set()
         for nb_turns in range(constants.MAX_TURNS):
             print(
                 "...move {}, {} possibilities".format(
@@ -50,11 +48,12 @@ class Game:
             new_tries = []
             for current_try in current_tries:
                 successful_try = current_try.gen_leafs(
-                    goal_position, goal, main_robot, other_robots, seen_list
+                    goal_position, goal, robots, seen_positions
                 )
                 if successful_try:
                     successful_try.result_board.display(self.goal_positions)
-                    return successful_try.result_board, nb_turns
+                    print("...resolved in {} turns".format(nb_turns + 1))
+                    return successful_try.result_board
                 new_tries.extend(current_try.leafs)
             current_tries = new_tries
         raise ValueError("No solution found in {}".format(nb_turns + 1))
